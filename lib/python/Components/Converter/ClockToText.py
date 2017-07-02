a140c8bacadd1619498271c57255d1c6d16e784e
MastaG <mastag@gmail.com>
2017-07-02 02:46:18 +0200

    Fix conflicts

--- lib/python/Components/Converter/ClockToText.py
+++ lib/python/Components/Converter/ClockToText.py
@@ -3,67 +3,16 @@ from Converter import Converter
 from Components.Element import cached
 
 class ClockToText(Converter, object):
-<<<<<<< HEAD
-=======
-	DEFAULT = 0
-	WITH_SECONDS = 1
-	IN_MINUTES = 2
-	DATE = 3
-	FORMAT = 4
-	AS_LENGTH = 5
-	TIMESTAMP = 6
-	FULL = 7
-	SHORT_DATE = 8
-	LONG_DATE = 9
-	VFD = 10
-	AS_LENGTHHOURS = 11
-	AS_LENGTHSECONDS = 12
 	FULL_DATE = 13
 
-	# add: date, date as string, weekday, ...
-	# (whatever you need!)
-
->>>>>>> upstream/develop
 	def __init__(self, type):
 		Converter.__init__(self, type)
 
 		self.type = type
 		self.fix = ""
-<<<<<<< HEAD
 		if ";" in type:
 			type, self.fix = type.split(";")
 		if "Format" in type:
-=======
-		if ';' in type:
-			type, self.fix = type.split(';')
-
-		if type == "WithSeconds":
-			self.type = self.WITH_SECONDS
-		elif type == "InMinutes":
-			self.type = self.IN_MINUTES
-		elif type == "Date":
-			self.type = self.DATE
-		elif type == "AsLength":
-			self.type = self.AS_LENGTH
-		elif type == "AsLengthHours":
-			self.type = self.AS_LENGTHHOURS
-		elif type == "AsLengthSeconds":
-			self.type = self.AS_LENGTHSECONDS
-		elif type == "Timestamp":
-			self.type = self.TIMESTAMP
-		elif type == "Full":
-			self.type = self.FULL
-		elif type == "ShortDate":
-			self.type = self.SHORT_DATE
-		elif type == "LongDate":
-			self.type = self.LONG_DATE
-		elif type == "FullDate":
-			self.type = self.FULL_DATE
-		elif type == "VFD":
-			self.type = self.VFD
-		elif "Format" in type:
-			self.type = self.FORMAT
->>>>>>> upstream/develop
 			self.fmt_string = type[7:]
 
 	@cached
@@ -117,20 +66,12 @@ class ClockToText(Converter, object):
 		elif self.type == "LongDate":
 			# TRANSLATORS: long date representations dayname daynum monthname in strftime() format! See 'man strftime'
 			d = _("%A %e %B")
-<<<<<<< HEAD
-		elif self.type == "VFD":
-			# VFD hour minute
-			return "%02d%02d" % (t.tm_hour, t.tm_min)
-=======
 		elif self.type == self.FULL_DATE:
 			# TRANSLATORS: full date representations short dayname daynum monthname long year in strftime() format! See 'man strftime'
 			d = _("%a %e %B %Y")
-		elif self.type == self.VFD:
-			# TRANSLATORS: VFD hour:minute daynum short monthname in strftime() format! See 'man strftime'
-			d = _("%k:%M %e/%m")
-		elif self.type == self.FORMAT:
-			d = self.fmt_string
->>>>>>> upstream/develop
+		elif self.type == "VFD":
+			# VFD hour minute
+			return "%02d%02d" % (t.tm_hour, t.tm_min)
 		else:
 			# TRANSLATORS: default time format hour:minute
 			return fix_space(_("%2d:%02d") % (t.tm_hour, t.tm_min))

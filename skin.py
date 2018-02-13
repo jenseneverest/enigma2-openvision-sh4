422fa6c2d433f0aa51d4034e54068a4ade7c579f
MastaG <mastag@gmail.com>
2018-02-13 20:19:04 +0100

    Set Full HD PLi skin on spark as well

--- skin.py
+++ skin.py
@@ -75,7 +75,13 @@ def skin_user_skinname():
 
 # example: loadSkin("nemesis_greenline/skin.xml")
 config.skin = ConfigSubsection()
-DEFAULT_SKIN = "PLi-HD/skin.xml"
+DEFAULT_SKIN = SystemInfo["HasFullHDSkinSupport"] and "PLi-FullNightHD/skin.xml" or "PLi-HD/skin.xml"
+# on SD hardware, PLi-HD will not be available
+if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
+	# in that case, fallback to Magic (which is an SD skin)
+	DEFAULT_SKIN = "Magic/skin.xml"
+	if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
+		DEFAULT_SKIN = "skin.xml"
 config.skin.primary_skin = ConfigText(default=DEFAULT_SKIN)
 
 profile("LoadSkin")

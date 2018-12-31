2c45aed0aa870e827bde96a4345651b18dec64a4
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2018-12-31 12:32:11 +0100

    bsod.cpp: Update for openvision patch

--- main/bsod.cpp
+++ main/bsod.cpp
@@ -155,7 +155,7 @@ void bsodFatal(const char *component)
 		strftime(tm_str, sizeof(tm_str), "%a %b %_d %T %Y", &tm);
 
 		fprintf(f,
-			"Taapat based on OpenPLi Enigma2 crash log\n\n"
+			"OpenPLi Enigma2 crash log\n\n"
 			"crashdate=%s\n"
 			"compiledate=%s\n"
 			"skin=%s\n"

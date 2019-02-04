ea419fe138e336edbe9c193231aa1af338fb0969
MastaG <mastag@gmail.com>
2019-02-04 11:09:50 +0100

    Fix driver date
    It only displays the git revsion because thats the only thing we have in: /var/lib/opkg/info/fulan-dvb-modules.control

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -127,15 +127,22 @@ def getDriverInstalledDate():
 	try:
 		from glob import glob
 		try:
-			driver = [x.split("-")[-2:-1][0][-8:] for x in open(glob("/var/lib/opkg/info/*-dvb-modules-*.control")[0], "r") if x.startswith("Version:")][0]
-			return  "%s-%s-%s" % (driver[:4], driver[4:6], driver[6:])
+			driver = [x.split("Version:") for x in open(glob("/var/lib/opkg/info/fulan-dvb-modules.control")[0], "r") if x.startswith("Version:")][0]
+			return  "%s" % driver[1].replace("\n","")
 		except:
 			try:
-				driver = [x.split("Version:") for x in open(glob("/var/lib/opkg/info/*-dvb-proxy-*.control")[0], "r") if x.startswith("Version:")][0]
-				return  "%s" % driver[1].replace("\n","")
+				driver = [x.split("-")[-2:-1][0][-8:] for x in open(glob("/var/lib/opkg/info/*-dvb-modules-*.control")[0], "r") if x.startswith("Version:")][0]
+				return  "%s-%s-%s" % (driver[:4], driver[4:6], driver[6:])
 			except:
-				driver = [x.split("Version:") for x in open(glob("/var/lib/opkg/info/*-platform-util-*.control")[0], "r") if x.startswith("Version:")][0]
-				return  "%s" % driver[1].replace("\n","")
+				try:
+					driver = [x.split("Version:") for x in open(glob("/var/lib/opkg/info/*-dvb-proxy-*.control")[0], "r") if x.startswith("Version:")][0]
+					return  "%s" % driver[1].replace("\n","")
+				except:
+					try:
+						driver = [x.split("Version:") for x in open(glob("/var/lib/opkg/info/*-platform-util-*.control")[0], "r") if x.startswith("Version:")][0]
+						return  "%s" % driver[1].replace("\n","")
+					except:
+						return _("unknown")
 	except:
 		return _("unknown")
 

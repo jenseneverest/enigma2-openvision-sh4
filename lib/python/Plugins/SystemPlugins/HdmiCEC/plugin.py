05efeeae4401650138f0591ba35921e2745bfb70
Persian Prince <persianpros@yahoo.com>
2019-02-23 11:16:05 +0330

    Restore HDMICEC

--- lib/python/Plugins/SystemPlugins/HdmiCEC/plugin.py
+++ lib/python/Plugins/SystemPlugins/HdmiCEC/plugin.py
@@ -147,7 +147,7 @@ def startSetup(menuid):
 
 def Plugins(**kwargs):
 	from os import path
-	if path.exists("/dev/hdmi_cec") and path.exists("/lib/modules/cec.ko"):
+	if path.exists("/dev/hdmi_cec") or path.exists("/dev/misc/hdmi_cec0"):
 		import Components.HdmiCec
 		from Plugins.Plugin import PluginDescriptor
 		return [PluginDescriptor(where = PluginDescriptor.WHERE_MENU, fnc = startSetup)]

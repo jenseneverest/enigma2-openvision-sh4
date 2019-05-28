e7b38bb84e4e6de921137b50b6f1a36a6923efd9
Persian Prince <persianpros@yahoo.com>
2019-05-28 19:28:41 +0430

    OScamInfo.py: Support NCam which is Open Source now thanks to fairbird

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -112,3 +112,6 @@ SystemInfo["HaveCISSL"] = fileCheck("/etc/ssl/certs/customer.pem") and fileCheck
 SystemInfo["CanChangeOsdAlpha"] = open("/proc/stb/video/alpha", "r") and True or False
 SystemInfo["ScalerSharpness"] = fileExists("/proc/stb/vmpeg/0/pep_scaler_sharpness")
 SystemInfo["OScamInstalled"] = fileExists("/usr/bin/oscam") or fileExists("/usr/bin/oscam-emu")
+SystemInfo["OScamIsActive"] = SystemInfo["OScamInstalled"] and fileExists("/tmp/.oscam/oscam.version")
+SystemInfo["NCamInstalled"] = fileExists("/usr/bin/ncam")
+SystemInfo["NCamIsActive"] = SystemInfo["NCamInstalled"] and fileExists("/tmp/.ncam/ncam.version")

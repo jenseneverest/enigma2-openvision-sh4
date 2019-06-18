d866076db0350b68db150fcceaf09c4761b3ce36
Persian Prince <persianpros@yahoo.com>
2019-06-18 19:56:51 +0430

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -202,16 +202,16 @@ class CommitInfo(Screen):
 
 		self.project = 0
 		self.projects = [
-			("https://api.github.com/repos/openpli/enigma2/commits" + branch, "Enigma2 - PLi"),
 			("https://api.github.com/repos/OpenVisionE2/enigma2-openvision-sh4/commits" + branch, "Enigma2 - SH4"),
-			("https://api.github.com/repos/openpli/openpli-oe-core/commits" + branch, "OE - PLi"),
-			("https://api.github.com/repos/OpenVisionE2/openvision-oe/commits" + branch, "OE - Vision"),
+			("https://api.github.com/repos/OpenVisionE2/openvision-oe/commits", "OE - Vision 7.x"),
+			("https://api.github.com/repos/OpenVisionE2/openvision-development-platform/commits", "OE - Vision 9.x"),
 			("https://api.github.com/repos/OpenVisionE2/sh4-driver/commits", "SH4 Driver"),
+			("https://api.github.com/repos/OpenVisionE2/sh4-apps/commits", "SH4 Apps"),
 			("https://api.github.com/repos/OpenVisionE2/servicemp3epl/commits", "Service MP3 EPlayer"),
+			("https://api.github.com/repos/OpenVisionE2/gstreamer1.0-plugin-multibox-dvbmediasink/commits", "DVB MediaSink"),
 			("https://api.github.com/repos/openpli/enigma2-plugins/commits", "Enigma2 Plugins"),
 			("https://api.github.com/repos/OpenVisionE2/alliance-plugins/commits", "Alliance Plugins"),
-			("https://api.github.com/repos/E2OpenPlugins/e2openplugin-OpenWebif/commits", "Open WebIF"),
-			("https://api.github.com/repos/OpenVisionE2/BackupSuite/commits", "Backup Suite")
+			("https://api.github.com/repos/E2OpenPlugins/e2openplugin-OpenWebif/commits", "Open WebIF")
 		]
 		self.cachedProjects = {}
 		self.Timer = eTimer()

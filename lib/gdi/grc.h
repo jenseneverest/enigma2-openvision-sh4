75d1deb91f6fb9d5f356539f199cd21feedc5976
persianpros <persianpros@yahoo.com>
2019-09-21 05:55:12 +0430

    Revert "Set the marked position during render"
    This reverts commit a5a628502d4a0f9f83fe91dbf3e55cf24fb5d257.

--- lib/gdi/grc.h
+++ lib/gdi/grc.h
@@ -86,8 +86,6 @@ struct gOpcode
 			int flags;
 			int border;
 			gRGB bordercolor;
-			int markedpos;
-			int scrollpos;
 		} *renderText;
 
 		struct prenderPara
@@ -234,7 +232,7 @@ public:
 
 		RT_WRAP = 64
 	};
-	void renderText(const eRect &position, const std::string &string, int flags=0, gRGB bordercolor=gRGB(), int border=0, int markedpos=-1);
+	void renderText(const eRect &position, const std::string &string, int flags=0, gRGB bordercolor=gRGB(), int border=0);
 
 	void renderPara(eTextPara *para, ePoint offset=ePoint(0, 0));
 
@@ -292,7 +290,6 @@ protected:
 	gRGB m_foreground_color_rgb, m_background_color_rgb;
 	ePtr<gFont> m_current_font;
 	ePoint m_current_offset;
-	int m_text_offset;
 
 	std::stack<gRegion> m_clip_stack;
 	gRegion m_current_clip;

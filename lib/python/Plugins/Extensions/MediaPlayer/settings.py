3f4ae7d5ab97d563116acafd7a4a404bbd350438
Persian Prince <persianpros@yahoo.com>
2019-10-10 20:14:47 +0330

    Update settings.py

--- lib/python/Plugins/Extensions/MediaPlayer/settings.py
+++ lib/python/Plugins/Extensions/MediaPlayer/settings.py
@@ -93,6 +93,7 @@ class MediaPlayerSettings(Screen, ConfigListScreen):
 			self.list.append(getConfigListEntry(_("Sorting of playlists"), config.mediaplayer.sortPlaylists))
 			self.list.append(getConfigListEntry(_("Always hide infobar"), config.mediaplayer.alwaysHideInfoBar))
 			self.list.append(getConfigListEntry(_("Show media player on main menu"), config.mediaplayer.onMainMenu))
+			self.list.append(getConfigListEntry(_("Play file when add in empty playlist"), config.mediaplayer.playIfEmptyPlaylists))
 			self["config"].setList(self.list)
 		except KeyError:
 			print "keyError"

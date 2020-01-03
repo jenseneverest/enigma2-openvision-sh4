96d2fb7f40fc84d487e72d24994cf77cd284c2b6
captain <captain.onboard@web.de>
2020-01-03 09:04:33 +0100

    try fix for Twisted Web: problems encoding unicode

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from config import config, ConfigSubsection, ConfigSlider, ConfigYesNo, ConfigNothing
 from enigma import eDBoxLCD
 from Components.SystemInfo import SystemInfo

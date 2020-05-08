59438afb094c0a1a45f748093564f96fa302f7be
persianpros <persianpros@yahoo.com>
2020-05-09 00:17:55 +0430

    NimManager.py: Get rid of hw variable

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -173,7 +173,6 @@ class SecConfigure:
 		for slot in nim_slots:
 			x = slot.slot
 			nim = slot.config
-			hw = getBoxType()
 			if slot.isCompatible("DVB-S"):
 				print("[NimManager] slot: " + str(x) + " configmode: " + str(nim.configMode.value))
 				if nim.configMode.value in ( "loopthrough", "satposdepends", "nothing" ):
@@ -1188,8 +1187,6 @@ def InitSecParams():
 # the configElement should be only visible when diseqc 1.2 is disabled
 
 def InitNimManager(nimmgr, update_slots = []):
-	hw = getBoxType()
-
 	if not hasattr(config, "Nims"):
 		InitSecParams()
 		config.Nims = ConfigSubList()

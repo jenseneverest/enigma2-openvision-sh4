3e6d5053d87c48329468765e3e9c3405e9cc461a
persianpros <persianpros@yahoo.com>
2020-07-01 17:31:45 +0430

    Be more compatible with python 3

--- mytest.py
+++ mytest.py
@@ -136,7 +136,7 @@ try:
 
 	def runReactor():
 		reactor.run(installSignalHandlers=False)
-except ImportError:
+except ImportError as e:
 	print("[mytest] twisted not available")
 	def runReactor():
 		enigma.runMainloop()

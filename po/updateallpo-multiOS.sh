3a056f888d1168552048a9db4847afac1987c24c
persianpros <persianpros@gmail.com>
2020-10-18 13:06:27 +0330

    xml2po.py: Be python 3 friendly

--- po/updateallpo-multiOS.sh
+++ po/updateallpo-multiOS.sh
@@ -110,10 +110,6 @@ done
 #
 command -v "$python" >/dev/null 2>&1 || { printf >&2 "Script requires python but it's not installed.  Aborting."; \
 		 printf "Please download latest version and install it from: https://www.python.org/\n"; exit 1; }
-ver=$("$python" -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
-if [ "$ver" -ge "30" ]; then
-   xml2po="xml2po-python3.py"
-fi
 printf "Python used [%s] script used [%s]: " "$python" "$xml2po"
 "$python" --version
 #
@@ -131,7 +127,7 @@ if [[ "$OSTYPE" == "darwin"* ]]
     	findoptions=" -s -X "
 fi
 #
-# Script only run with gsed but on some distro normal sed is already gsed so checking it.
+# Script only run with sed but on some distro normal sed is already sed so checking it.
 #
 sed --version 2> /dev/null | grep -q "GNU"
 if [ $? -eq 0 ]; then
